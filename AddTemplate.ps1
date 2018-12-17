4f29eb8ab1e12340e57dd256961fbf0567cbaf6e
Ryan Butler <ryan.butler@corp.thinkahead.root>
2018-12-16 22:48:43 -0600

    adds

---
+++ AddTemplate.ps1
@@ -0,0 +1,27 @@
+﻿[xml]$XML = get-content ".\Citrix_Windows_10_1709.xml"
+[xml]$mpxml = get-content ".\communitymarketplace.xml"
+
+$metadata = $xml.root.metadata
+
+$newtemplate = $mpxml.root.templates.AppendChild($mpxml.CreateElement("template"))
+
+$newtemp = [ordered]@{
+id = $metadata.id
+version = $metadata.version
+displayname = $metadata.displayname
+description = $metadata.description
+category = $metadata.category
+author = $metadata.author
+updatedate  = $metadata.lastupdatedate
+url = "https://ctxsym.citrix.com/supportabilitytools/citrixoptimizer/templates/Citrix_Windows_10_1607.xml"
+checksum = "" 
+beta = "true"
+}
+
+$newtemp.Keys | % {
+write-host $_
+$newXmlElement = $newtemplate.AppendChild($mpxml.CreateElement($_));
+$newXmlTextNode = $newXmlElement.AppendChild($mpxml.CreateTextNode($newtemp.Item($_)));
+}
+
+$mpxml.Save(".\communitymarketplace3.xml")
