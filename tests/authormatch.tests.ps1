3a3b35c1758d307c3c2e3e69d85e4f9c94fa0efe
Ryan Butler <ryan.butler@corp.thinkahead.root>
2018-12-19 17:15:06 -0600

    change to test

--- tests/authormatch.tests.ps1
+++ tests/authormatch.tests.ps1
@@ -1,4 +1,36 @@
+function Test-XMLFile {
+    <#
+    .SYNOPSIS
+    Test the validity of an XML file
+    #>
+    [CmdletBinding()]
+    param (
+    [parameter(mandatory=$true)][ValidateNotNullorEmpty()][string]$xmlFilePath
+    )
+    
+    # Check the file exists
+    if (!(Test-Path -Path $xmlFilePath)){
+    throw "$xmlFilePath is not valid. Please provide a valid path to the .xml fileh"
+    }
+    # Check for Load or Parse errors when loading the XML file
+    $xml = New-Object System.Xml.XmlDocument
+    try {
+    $xml.Load((Get-ChildItem -Path $xmlFilePath).FullName)
+    return $true
+    }
+    catch [System.Xml.XmlException] {
+    Write-Verbose "$xmlFilePath : $($_.toString())"
+    return $false
+    }
+    }
+
 $templates = Get-ChildItem -Path ".\templates\*.xml" -Recurse -Force
+
+$testCase = $templates | Foreach-Object{@{file=$_}}         
+    It "Template <file> should be valid XML" -TestCases $testCase {
+        param($file)
+        Test-XMLFile $file.FullName|should -be "True"
+    }
 
 $testCase = $templates | Foreach-Object{@{file=$_}}         
     It "Template <file> directory should match author" -TestCases $testCase {
