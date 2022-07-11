38dfae1ab093bb155cc599448f66b7a09a4965d4
Ryan Butler <ryancbutler@users.noreply.github.com>
2022-07-10 21:41:50 -0500

    F/pester (#16)
    * fix test
    
    * update

--- tests/authormatch.tests.ps1
+++ tests/authormatch.tests.ps1
@@ -1,38 +1,42 @@
-function Test-XMLFile {
-    <#
+BeforeAll {
+    function Test-XMLFile {
+        <#
     .SYNOPSIS
     Test the validity of an XML file
     #>
-    [CmdletBinding()]
-    param (
-    [parameter(mandatory=$true)][ValidateNotNullorEmpty()][string]$xmlFilePath
-    )
+        [CmdletBinding()]
+        param (
+            [parameter(mandatory = $true)][ValidateNotNullorEmpty()][string]$xmlFilePath
+        )
     
-    # Check the file exists
-    if (!(Test-Path -Path $xmlFilePath)){
-    throw "$xmlFilePath is not valid. Please provide a valid path to the .xml fileh"
+        # Check the file exists
+        if (!(Test-Path -Path $xmlFilePath)) {
+            throw "$xmlFilePath is not valid. Please provide a valid path to the .xml fileh"
+        }
+        # Check for Load or Parse errors when loading the XML file
+        $xml = New-Object System.Xml.XmlDocument
+        try {
+            $xml.Load((Get-ChildItem -Path $xmlFilePath).FullName)
+            return $true
+        }
+        catch [System.Xml.XmlException] {
+            Write-Verbose "$xmlFilePath : $($_.toString())"
+            return $false
+        }
     }
-    # Check for Load or Parse errors when loading the XML file
-    $xml = New-Object System.Xml.XmlDocument
-    try {
-    $xml.Load((Get-ChildItem -Path $xmlFilePath).FullName)
-    return $true
-    }
-    catch [System.Xml.XmlException] {
-    Write-Verbose "$xmlFilePath : $($_.toString())"
-    return $false
-    }
-    }
+    
+    $templates = Get-ChildItem -Path ".\templates\*.xml" -Recurse -Force
+}
 
-$templates = Get-ChildItem -Path ".\templates\*.xml" -Recurse -Force
 
-$testCase = $templates | Foreach-Object{@{file=$_}}         
+Describe "XML Formatting" {
+    $testCase = $templates | Foreach-Object { @{file = $_ } }         
     It "Template <file> should be valid XML" -TestCases $testCase {
         param($file)
-        Test-XMLFile $file.FullName|should -be "True"
+        Test-XMLFile $file.FullName | should -be "True"
     }
 
-$testCase = $templates | Foreach-Object{@{file=$_}}         
+    $testCase = $templates | Foreach-Object { @{file = $_ } }         
     It "Template <file> directory should match author" -TestCases $testCase {
         param($file)
         [xml]$XML = get-content $file.FullName
@@ -40,24 +44,25 @@ $testCase = $templates | Foreach-Object{@{file=$_}}
         $file.directory.name | should -Match $metadata.author
     }
 
-$testCase = $templates | Foreach-Object{@{file=$_}}         
+    $testCase = $templates | Foreach-Object { @{file = $_ } }         
     It "Template <file> should be schemaversion 2.0 or greater" -TestCases $testCase {
         param($file)
         [xml]$XML = get-content $file.FullName
         $metadata = $xml.root.metadata
-        $metadata.schemaversion| should -BeGreaterOrEqual 2
+        $metadata.schemaversion | should -BeGreaterOrEqual 2
     }
 
-$testCase = $templates | Foreach-Object{@{file=$_}}         
+    $testCase = $templates | Foreach-Object { @{file = $_ } }         
     It "Template <file> should have a valid date format (M/DD/YYYY)" -TestCases $testCase {
         param($file)
         [xml]$XML = get-content $file.FullName
         $metadata = $xml.root.metadata
         #(test-date $metadata.lastupdatedate).test| Should -be "True"
-        [DateTime]::ParseExact($metadata.lastupdatedate,'M\/dd\/yyyy',$null)
+        [DateTime]::ParseExact($metadata.lastupdatedate, 'M\/dd\/yyyy', $null)
     }
+}
 
-Describe "Duplicate XML file name check "{
+Describe "Duplicate XML file name check " {
     it "Check for unique template file names" {
         $templatecount = $templates | Group-Object name | Where-Object count -gt 1
         $templatecount.count | should -Not -BeGreaterThan 0
--- tests/uniquetemplates.tests.ps1
+++ tests/uniquetemplates.tests.ps1
@@ -1,57 +1,54 @@
-﻿function Test-XMLFile {
-<#
-.SYNOPSIS
-Test the validity of an XML file
-#>
-[CmdletBinding()]
-param (
-[parameter(mandatory=$true)][ValidateNotNullorEmpty()][string]$xmlFilePath
-)
+﻿
+function Test-XMLFile {
+    #Test the validity of an XML file
+    [CmdletBinding()]
+    param (
+        [parameter(mandatory = $true)][ValidateNotNullorEmpty()][string]$xmlFilePath
+    )
 
-# Check the file exists
-if (!(Test-Path -Path $xmlFilePath)){
-throw "$xmlFilePath is not valid. Please provide a valid path to the .xml fileh"
+    # Check the file exists
+    if (!(Test-Path -Path $xmlFilePath)) {
+        throw "$xmlFilePath is not valid. Please provide a valid path to the .xml fileh"
+    }
+    # Check for Load or Parse errors when loading the XML file
+    $xml = New-Object System.Xml.XmlDocument
+    try {
+        $xml.Load((Get-ChildItem -Path $xmlFilePath).FullName)
+        return $true
+    }
+    catch [System.Xml.XmlException] {
+        Write-Verbose "$xmlFilePath : $($_.toString())"
+        return $false
+    }
 }
-# Check for Load or Parse errors when loading the XML file
-$xml = New-Object System.Xml.XmlDocument
-try {
-$xml.Load((Get-ChildItem -Path $xmlFilePath).FullName)
-return $true
-}
-catch [System.Xml.XmlException] {
-Write-Verbose "$xmlFilePath : $($_.toString())"
-return $false
-}
-}
+
 
 Describe 'Check for valid XML' {
     it 'communitymarketplace.xml should be valid XML' {
-        Test-XMLFile "$PSScriptRoot\..\communitymarketplace.xml"|should -be "True"
+        Test-XMLFile "$PSScriptRoot\..\communitymarketplace.xml" | should -be "True"
     }
-
 }
-
 
 [xml]$mpxml = get-content "$PSScriptRoot\..\communitymarketplace.xml"
 
 
 $displaynames = @()
-Select-Xml -Xml $mpxml -XPath "//displayname"|%{$displaynames += $_.Node.InnerText}
+Select-Xml -Xml $mpxml -XPath "//displayname" | % { $displaynames += $_.Node.InnerText }
 
 $ids = @()
-Select-Xml -Xml $mpxml -XPath "//id"|%{$ids += $_.Node.InnerText}
+Select-Xml -Xml $mpxml -XPath "//id" | % { $ids += $_.Node.InnerText }
 
 
 Describe 'Checking for duplicate template entries' {      
-        it 'XML should only contain unique displaynames' {
-            $dups = ((($displaynames | group | ?{$_.Count -gt 1}).Values))
-            write-host $dups
-            $dups.count | Should -not -BeGreaterThan 0
-        }
-        it 'XML should only contain unique ids' {
-            $dups = ((($ids | group | ?{$_.Count -gt 1}).Values))
-            write-host $dups
-            $dups.count | Should -not -BeGreaterThan 0
-        }
+    it 'XML should only contain unique displaynames' {
+        $dups = ((($displaynames | group | ? { $_.Count -gt 1 }).Values))
+        write-host $dups
+        $dups.count | Should -not -BeGreaterThan 0
+    }
+    it 'XML should only contain unique ids' {
+        $dups = ((($ids | group | ? { $_.Count -gt 1 }).Values))
+        write-host $dups
+        $dups.count | Should -not -BeGreaterThan 0
+    }
 
 }
