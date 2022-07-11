1785dbe217a9bcd02aaa299b6cd39cd93e456e4d
Ryan Butler <ryancbutler@users.noreply.github.com>
2022-07-10 22:03:45 -0500

    Update authormatch.tests.ps1

--- tests/authormatch.tests.ps1
+++ tests/authormatch.tests.ps1
@@ -1,26 +1,29 @@
-function Test-XMLFile {
-#Test the validity of an XML file
-[CmdletBinding()]
-param (
-    [parameter(mandatory = $true)][ValidateNotNullorEmpty()][string]$xmlFilePath
-)
+BeforeAll {
+    function Test-XMLFile {
+    #Test the validity of an XML file
+    [CmdletBinding()]
+    param (
+        [parameter(mandatory = $true)][ValidateNotNullorEmpty()][string]$xmlFilePath
+    )
 
-    # Check the file exists
-    if (!(Test-Path -Path $xmlFilePath)) {
-        throw "$xmlFilePath is not valid. Please provide a valid path to the .xml fileh"
+        # Check the file exists
+        if (!(Test-Path -Path $xmlFilePath)) {
+            throw "$xmlFilePath is not valid. Please provide a valid path to the .xml fileh"
+        }
+        # Check for Load or Parse errors when loading the XML file
+        $xml = New-Object System.Xml.XmlDocument
+        try {
+            $xml.Load((Get-ChildItem -Path $xmlFilePath).FullName)
+            return $true
+        }
+        catch [System.Xml.XmlException] {
+            Write-Verbose "$xmlFilePath : $($_.toString())"
+            return $false
+        }
     }
-    # Check for Load or Parse errors when loading the XML file
-    $xml = New-Object System.Xml.XmlDocument
-    try {
-        $xml.Load((Get-ChildItem -Path $xmlFilePath).FullName)
-        return $true
-    }
-    catch [System.Xml.XmlException] {
-        Write-Verbose "$xmlFilePath : $($_.toString())"
-        return $false
-    }
+
 }
-    
+
 $templates = Get-ChildItem -Path ".\templates\*.xml" -Recurse -Force
 
 Describe "XML Formatting" {
