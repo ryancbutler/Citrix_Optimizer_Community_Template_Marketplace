ce130d162377073cfdb5935a7f2cca8aa049170a
Ryan Butler <ryancbutler@users.noreply.github.com>
2022-07-10 22:05:56 -0500

    Update uniquetemplates.tests.ps1

--- tests/uniquetemplates.tests.ps1
+++ tests/uniquetemplates.tests.ps1
@@ -1,24 +1,25 @@
-﻿
-function Test-XMLFile {
-    #Test the validity of an XML file
-    [CmdletBinding()]
-    param (
-        [parameter(mandatory = $true)][ValidateNotNullorEmpty()][string]$xmlFilePath
-    )
+BeforeAll {
+    function Test-XMLFile {
+        #Test the validity of an XML file
+        [CmdletBinding()]
+        param (
+            [parameter(mandatory = $true)][ValidateNotNullorEmpty()][string]$xmlFilePath
+        )
 
-    # Check the file exists
-    if (!(Test-Path -Path $xmlFilePath)) {
-        throw "$xmlFilePath is not valid. Please provide a valid path to the .xml fileh"
-    }
-    # Check for Load or Parse errors when loading the XML file
-    $xml = New-Object System.Xml.XmlDocument
-    try {
-        $xml.Load((Get-ChildItem -Path $xmlFilePath).FullName)
-        return $true
-    }
-    catch [System.Xml.XmlException] {
-        Write-Verbose "$xmlFilePath : $($_.toString())"
-        return $false
+        # Check the file exists
+        if (!(Test-Path -Path $xmlFilePath)) {
+            throw "$xmlFilePath is not valid. Please provide a valid path to the .xml fileh"
+        }
+        # Check for Load or Parse errors when loading the XML file
+        $xml = New-Object System.Xml.XmlDocument
+        try {
+            $xml.Load((Get-ChildItem -Path $xmlFilePath).FullName)
+            return $true
+        }
+        catch [System.Xml.XmlException] {
+            Write-Verbose "$xmlFilePath : $($_.toString())"
+            return $false
+        }
     }
 }
 
